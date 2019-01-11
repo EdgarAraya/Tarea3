78f017615250817169b9b3d546cceed80cdde05c
Luis <Luis@DESKTOP-0KQ9I2J.iptv.microsoft.com>
2019-01-11 19:04:48 -0300

    jdialoa

--- src/tarea/pkg3/Util.java
+++ src/tarea/pkg3/Util.java
@@ -6,9 +6,11 @@
 package tarea.pkg3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -16,6 +18,12 @@ import java.io.IOException;
  * @author Luis
  */
 public class Util {
+    
+    /**
+     * Carga Jugadores a Juego
+     *@param archivo, ruta del archivo en String
+     * @return lista con jugadores cargados
+     */
     
     public  ListaJugadores cargarJugadores(String archivo) throws FileNotFoundException, IOException{
        String k;
@@ -34,8 +42,30 @@ public class Util {
         return lista;
     }
     
-    public static boolean guardarTiempo(Jugador jugador){
+    /**
+     * Guarda tiempos
+     * @param ar, String con ruta
+     * @param lista, Lista a la cual agregar tiempos
+     * @return true en caso de que se guardo, false en caso de que no
+     */
+    
+    public static boolean guardarTiempo(String ar, ListaJugadores lista){
+        try(BufferedWriter bw = new BufferedWriter (new FileWriter(ar))){
+            for(Jugador alguno:lista.getJugadores()){
+                bw.write(alguno.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }catch(IOException e){
+            
+        }
         return false;
-        
-    }
+    
+}
+   
+    
+    
+    
+    
 }
